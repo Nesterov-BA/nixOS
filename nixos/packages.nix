c86b621da420a95c70fb6a11b409d09f08b69f50
Nesterov-BA <nesterov.boris123@gmail.com>
2025-09-23 04:17:33 +0000

    added fonts

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -1,41 +1,68 @@
 { pkgs, config, ... }: {
   environment.systemPackages = with pkgs; [
-    neovim
-    marksman
-    lua-language-server
-    btop
-    neofetch
-    wl-clipboard
-    dunst
-    pavucontrol
-    stylua
-    wget
-    gh
-    nix-search-tv
-    cargo
-    unzip
-    home-manager
-    gcc
-    nodejs_24
-    ripgrep
-    fd
-    lazygit
-    zathura
-    texliveFull
-    python3
-    fzf
-    # wikiman
-    tldr
-    kitty
-    gnumake
-    firefox
-    hyprland
-    icu
-    wayland
-    waybar
-    yazi
-    awesome
     # rofi
+    # wikiman
+    awesome
+    btop
+    cargo
+    dunst
+    fd
+    firefox
+    fzf
+    gcc
+    gh
+    gnumake
+    home-manager
+    hyprland
+    hyprpaper
+    icu
+    kitty
+    lazygit
+    lua-language-server
+    marksman
+    neofetch
+    neovim
+    nix-search-tv
+    nodejs_24
+    pavucontrol
+    python3
+    ripgrep
     rofi-wayland
+    stylua
+    telegram-desktop
+    texliveFull
+    tldr
+    unzip
+    waybar
+    wayland
+    wget
+    wl-clipboard
+    yazi
+    zathura
   ];
+  services = {
+    flatpak = {
+      enable = true;
+
+      # List the Flatpak applications you want to install
+      # Use the official Flatpak application ID (e.g., from flathub.org)
+      # Examples:
+      packages = [
+        "app.zen_browser.zen"
+        "com.github.tchx84.Flatseal" # Manage flatpak permissions - should always have this
+        #"com.rtosta.zapzap"              # WhatsApp client
+        #"io.github.flattool.Warehouse"   # Manage flatpaks, clean data, remove flatpaks and deps
+        #"it.mijorus.gearlever"           # Manage and support AppImages
+        #"io.github.freedoom.Phase1"      #  Classic Doom FPS 1
+        #"io.github.freedoom.Phase2"      #  Classic Doom FPS 2
+        #"io.github.dvlv.boxbuddyrs"      #  Manage distroboxes
+        #"de.schmidhuberj.tubefeeder"     #watch YT videos
+
+        # Add other Flatpak IDs here, e.g., "org.mozilla.firefox"
+      ];
+
+      # Optional: Automatically update Flatpaks when you run nixos-rebuild swit ch
+      update.onActivation = true;
+    };
+  };
 }
